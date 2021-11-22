692b55b287bb0ecd142fb2c2511df03cecd7eef8
benlehuyvy176 <71539951+benlehuyvy176@users.noreply.github.com>
2021-11-22 14:11:24 +0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,139 +1,72 @@
+from numpy.core.fromnumeric import shape
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array, load_img, array_to_img
-from PIL import Image, ImageOps
-menu = ['Read Data', 'Show Webcam']
 
-choice = st.sidebar.selectbox('Money Classification', menu)
+model = tf.keras.models.load_model('Model_1.h5')
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+money_type = ['1000', '10000', '100000', '2000', '20000', '200000', '5000', '50000', '500000']
+  
 
-if choice=='Read Data':
-    st.title('Please upload your money picture!')
-    st.image('media\Vietnames-Dong-500-000-to-10-000.jpg')
+menu = ['camera', 'predict money']
+choice = st.sidebar.selectbox('Menu', menu)
 
-    class_names = ['1000','10000','100000','2000','20000','200000','5000','50000','500000']
-    @st.cache(allow_output_mutation=True)
-    def load_model():
-        model=tf.keras.models.load_model('Model.h5')
-        return model
-    with st.spinner('Model is being loaded..'):
-        model=load_model()
-
-    st.write("""
-            # Money Classification
-            """
-            )
-
-    filename = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
-    st.set_option('deprecation.showfileUploaderEncoding', False)
-    def import_and_predict(image_data, model):
-        size = (224,224)    
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-        image = np.asarray(image)
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+if choice == 'predict money':
+    image_upload = st.file_uploader('upload file', type = ['jpg', 'png', 'jpeg'])
+    if image_upload != None:
+        image_np = np.asarray(bytearray(image_upload.read()),dtype = np.uint8)
+        img = cv2.imdecode(image_np,1)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (224,224))
+        img = np.expand_dims(img, axis=0)
+        prediction = model.predict(img)
+        index = np.argmax(prediction[0])
+        money = money_type[index]
+        st.image(image_upload)
+        st.write('This is:', money)
         
-        img_reshape = img[np.newaxis,...]
-    
-        prediction = model.predict(img_reshape)
         
-        return prediction
-    if filename is None:
-        st.text("Please upload an image file")
-    else:
-        image = Image.open(filename)
-        st.image(image, use_column_width=True)
-        predictions = import_and_predict(image, model)
-        score = tf.nn.softmax(predictions[0])
-        st.write(predictions)
-        st.write(score)
-        print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
-    )
 
-elif choice=='Show Webcam':
-    st.title('Open your webcam')
-    st.warning('Webcam show on local computer ONLY')
-    class_names = ['1000','10000','100000','2000','20000','200000','5000','50000','500000']
-    @st.cache(allow_output_mutation=True)
-    def load_model():
-        model=tf.keras.models.load_model('Model.h5')
-        return model
-    with st.spinner('Model is being loaded..'):
-        model=load_model()
 
-    st.write("""
-            # Money Classification
-            """
-            )
-    show = st.checkbox('Show!')
+
+elif choice == 'camera':
+    cam = cv2.VideoCapture(0) # device 0. If not work, try with 1 or 2
+    run = st.checkbox('Show webcam')
+    capture_button = st.checkbox('Capture')
+
+    captured_image = np.array(None)
+
+    if not cam.isOpened():
+        raise IOError("Cannot open webcam")
+
     FRAME_WINDOW = st.image([])
-    camera = cv2.VideoCapture(0) # device 1/2
-
-    while show:
-        _, frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        FRAME_WINDOW.image(frame)
-    else:
-        camera.release()
-    
     while True:
-        try:
-            check, frame = webcam.read()
-            print(check) #prints true as long as the webcam is running
-            print(frame) #prints matrix values of each framecd 
-            cv2.imshow("Capturing", frame)
-            key = cv2.waitKey(1)
-            if key == ord('s'): 
-                cv2.imwrite(filename='saved_img.jpg', img=frame)
-                webcam.release()
-                img_new = cv2.imread('saved_img.jpg', cv2.IMREAD_GRAYSCALE)
-                img_new = cv2.imshow("Captured Image", img_new)
-                cv2.waitKey(1650)
-                cv2.destroyAllWindows()
-                print("Processing image...")
-                img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
-                print("Converting RGB image to grayscale...")
-                gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-                print("Converted RGB image to grayscale...")
-                # print("Resizing image to 28x28 scale...")
-                # img_ = cv2.resize(gray,(28,28))
-                # print("Resized...")
-                img_resized = cv2.imwrite(filename='saved_img-final.jpg', img=img_)
-                print("Image saved!")
-            
-                break
-            elif key == ord('q'):
-                print("Turning off camera.")
-                webcam.release()
-                print("Camera off.")
-                print("Program ended.")
-                cv2.destroyAllWindows()
-                break
+        ret, frame = cam.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.flip(frame, 1)
+        FRAME_WINDOW.image(frame)
         
-        except(KeyboardInterrupt):
-            print("Turning off camera.")
-            webcam.release()
-            print("Camera off.")
-            print("Program ended.")
-            cv2.destroyAllWindows()
-            break
+        # cv2.imshow('My App!', frame)
 
-        if filename is None:
-            st.text("Please take a picture")
-        else:
-            image = Image.open(filename)
-            st.image(image, use_column_width=True)
-            predictions = import_and_predict(image, model)
-            score = tf.nn.softmax(predictions[0])
-            st.write(predictions)
-            st.write(score)
-            print(
-            "This image most likely belongs to {} with a {:.2f} percent confidence."
-            .format(class_names[np.argmax(score)], 100 * np.max(score))
-        )
+        # key = cv2.waitKey(1) & 0xFF
+        # if key==ord("q"):
+        #     break
+        if capture_button:
+            captured_image = frame
+            break
+    cam.release()
+    #img = cv2.imdecode(captured_image,1)
+    #img = cv2.cvtColor(captured_image,cv2.COLOR_BGR2RGB)
+    img = cv2.resize(captured_image, (224,224))
+    img = np.expand_dims(img, axis=0)
+    prediction = model.predict(img)
+    index = np.argmax(prediction[0])
+    money = money_type[index]
+    #st.image(image_upload)
+    st.write('This is:', money)
+    #cv2.destroyAllWindows()
+    
